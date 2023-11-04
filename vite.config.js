56ff616078532660391ea12178bdca7a7dccb207
Kin <kinwang6632@hotmail.com>
2023-11-05 02:13:41 +0800

    Test2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'./',
+  build:{
+    outDir:'docs'
+  },
   plugins: [vue(),
   VitePWA({
     registerType:'autoUpdate',
