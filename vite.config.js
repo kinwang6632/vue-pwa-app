451a1ac46d8d12db03ccf6bde0199964aa66e708
Kin <kinwang6632@hotmail.com>
2023-11-05 02:03:46 +0800

    測試用

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [vue(),
   VitePWA({
     registerType:'autoUpdate',
-    manifest:{
-      icons:{
-        src:"/icons/"
-      }
-    },
     workbox:{
       runtimeCaching:[{
         urlPattern:({url}) => {
